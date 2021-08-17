e0848ee9f0bdccac9493b48b5a53efd2fb501fe1
bryano13 <bryano13@hotmail.com>
2021-08-16 22:45:50 -0500

    song controller modified (added a hash to query)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,8 @@ class SongsController < ApplicationController
 
     #  /api/v1/genres/:genre_name/random_song
     def random_song
-        @genre = Artist.where("cast(genres AS VARCHAR) LIKE ?", "%\"#{params[:genre_name]}\"%")
+        genre_dict = {chosen_genre: "%\"#{params[:genre_name]}\"%"}
+        @genre = Artist.where("cast(genres AS VARCHAR) LIKE :chosen_genre", genre_dict)
         @artist = @genre.sample
         @track = @artist.albums.sample.songs.sample
         render json: @track, root: 'data', adapter: :json, status: :ok
